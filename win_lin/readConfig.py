a6b2174b9d978f39a601a0ec37e441a34684e38d
Administrator <846341956@qq.com>
2020-08-10 21:01:56 +0800

    修改

--- win_lin/readConfig.py
+++ win_lin/readConfig.py
@@ -5,12 +5,13 @@
 # @FileName: readConfig.py
 # @Software: PyCharm
 import  configparser,os
-configparser.RawConfigParser()
+
 class readConfig(object):
 
     def __init__(self):
     # 属性（读取文件的参数）
-        self.conf = configparser.RawConfigParser()
+    #     self.conf = configparser.RawConfigParser()  #无中文
+        self.conf = configparser.RawConfigParser()#配置文件存在中文，尤其是密码等
         config_path = os.path.dirname(__file__) + '\\' + 'config.ini'
         self.conf.read(config_path, encoding='utf-8-sig')
     #获取配置文件ConfigPath下的一个子项：
